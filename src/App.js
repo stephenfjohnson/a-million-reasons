c2676220f61b09d5d8811a8d69e23f181a62bdeb
Stephen Johnson <sforrestj@gmail.com>
2020-12-05 16:39:28 -0800

    GA Take 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Form from "./Form";
 import Reasons from "./Reasons";
 import styled from "styled-components";
 import { ModalContext } from "./ModalContext";
-import ReactGA from "react-ga";
+// import ReactGA from "react-ga";
 
 const App = () => {
   const { handleModal } = useContext(ModalContext);
 
-  useEffect(() => {
-    ReactGA.initialize("G-0CEN6QXHK4");
-    ReactGA.pageview(window.location.pathname);
-  });
+  // useEffect(() => {
+  //   ReactGA.initialize("G-0CEN6QXHK4");
+  //   ReactGA.pageview(window.location.pathname);
+  // });
 
   return (
     <div className="App">
